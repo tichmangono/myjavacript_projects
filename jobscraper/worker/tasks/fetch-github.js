019f905c19b7febd21b44dcc5eecf4265be9a434
Tichakunda Mangono <tichmangono@gmail.com>
2020-10-27 01:48:35 -0400

    Filter senior roles from based on job title

--- jobscraper/worker/tasks/fetch-github.js
+++ jobscraper/worker/tasks/fetch-github.js
@@ -12,9 +12,13 @@ const setAsync = promisify(client.set).bind(client);
 const baseUrl = 'https://jobs.github.com/positions.json'
 
 async function fetchGithub() {
+    
+    console.log('fetching Github');
+
     let resultCount = 1, onPage = 0;
     const allJobs = [];
 
+    // fetch all pages
     while (resultCount > 0 ){
         const res = await fetch(`${baseUrl}?page=${onPage}`);
         const jobs = await res.json(); 
@@ -24,11 +28,32 @@ async function fetchGithub() {
         console.log("got :", resultCount); 
         onPage++;
         
-
     }
-    console.log("got", allJobs.length, "jobs");
-    const success = await setAsync('github', JSON.stringify(allJobs));
 
+    console.log("got", allJobs.length, "jobs total");
+
+    // filter algo
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+        //let isJunior = true;
+
+        // algo logc here
+        if (
+            jobTitle.includes('senior') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('manager') ||
+            jobTitle.includes('architect')
+            ){
+                return false;
+            }
+            return true;
+    })
+
+    console.log("filtered down to junior jobs ", jrJobs.length);
+
+    // set in redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+ 
     console.log({success});
 }
 
